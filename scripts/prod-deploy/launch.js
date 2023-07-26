4545f2bc12c5040ba85a57da42e923539f56dac3
ivan <ruvlol@mail.ru>
2023-07-26 21:29:18 +0400

    asd

--- scripts/prod-deploy/launch.js
+++ scripts/prod-deploy/launch.js
@@ -12,17 +12,10 @@ async function main () {
     minter = await ethers.getContractAt("MinterUpgradeable", "0xBFA35599c7AEbb0dAcE9b5aa3ca5f2a79624D8Eb");
     console.log("retro Address: ", retro.address)
 
-    // initial mint
-    tx = await retro.initialMint(owner.address)
-    await tx.wait()
-    console.log('initial mint of 50M RETRO to ' + owner.address)
-
-    console.log('/////////////////////////')
-    console.log('check up')
-    console.log('/////////////////////////')
-    
-    console.log('RETRO owner balance (should be 50000000000000000000000000): ' + await retro.balanceOf(owner.address))
-    console.log('RETRO initial minted (should be true): ' + await retro.initialMinted())
+    // pass univ3 permissions to feeHandler
+    // create gauges
+    // initialize minter
+   
 }
 
 main()
