cba8aa361f036efc5f3796a8bb2fa4e1ad16cf9b
Christian Holman <chrisholm2@yahoo.com>
2023-07-27 17:18:46 +0100

    add VeAirdrop

---
+++ contracts/VeAirdrop.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import {IERC20} from "./interfaces/IERC20.sol";
+import {IVotingEscrow} from "./interfaces/IVotingEscrow.sol";
+
+contract VeAirdrop {
+  IVotingEscrow public immutable votingEscrow;
+  IERC20 public immutable underlyingToken;
+  uint256 public immutable lockDuration;
+
+  constructor(IVotingEscrow _votingEscrow, IERC20 _underlyingToken, uint256 _lockDuration) {
+    votingEscrow = _votingEscrow;
+    underlyingToken = _underlyingToken;
+    lockDuration = _lockDuration;
+
+    underlyingToken.approve(address(votingEscrow), type(uint256).max);
+
+    // Sanity check
+    require(votingEscrow.token() == address(underlyingToken), "VeAirdrop: voting escrow token mismatch");
+  }
+
+  function airdrop(address[] memory recipients, uint256[] memory values) public returns (bool) {
+    require(recipients.length == values.length, "VeAirdrop: recipients and values length mismatch");
+    for (uint256 i = 0; i < recipients.length; i++) {
+      underlyingToken.transferFrom(msg.sender, address(this), values[i]);
+      votingEscrow.create_lock_for(values[i], lockDuration, recipients[i]);
+    }
+    return true;
+  }
+}
