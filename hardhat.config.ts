00f95922e0ba2bf9614805f9adfff6864c36c51d
Celeborn2BeAlive <laurent.noel.c2ba@gmail.com>
2025-01-26 11:20:16 +0100

    Update dependencies to use typescript and viem

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,21 +1,25 @@
-require("@nomiclabs/hardhat-waffle");
+import { HardhatUserConfig } from "hardhat/types";
 
-require("@openzeppelin/hardhat-upgrades");
+import "@nomiclabs/hardhat-waffle";
 
-require("@nomiclabs/hardhat-etherscan");
+import "@openzeppelin/hardhat-upgrades";
 
-require("@nomiclabs/hardhat-web3");
+import "hardhat-tracer";
+import "hardhat-gas-reporter";
 
-require("hardhat-tracer");
-require("hardhat-gas-reporter");
-
-require("@nomicfoundation/hardhat-foundry");
+import "@nomicfoundation/hardhat-verify";
+import "@nomicfoundation/hardhat-foundry";
+import "@nomicfoundation/hardhat-viem";
+import "@nomicfoundation/hardhat-ignition-viem";
 
 const { PRIVATEKEY, PRIVATEKEYSECRET, APIKEY, APIKEY_ZK } = require("./pvkey.js");
 
-module.exports = {
+const config: HardhatUserConfig = {
   // latest Solidity version
   gasReporter: {
+    enabled: true
+  },
+  sourcify: {
     enabled: true
   },
   solidity: {
@@ -87,14 +91,13 @@ module.exports = {
     hardhat: {
       forking: {
         url: "https://polygon-mainnet.infura.io/v3/28b6e1b06d8b4cbcaaf8d7065ee116f3",
-        chainId: 137,
       },
       //accounts: []
     },
   },
 
   etherscan: {
-    apiKey: { polygon: APIKEY, "polygon-zkevm": APIKEY_ZK },
+    apiKey: { polygon: APIKEY, "polygon-zkevm": APIKEY_ZK, zetachain: APIKEY },
     customChains: [
       {
         network: "polygon",
@@ -127,3 +130,4 @@ module.exports = {
     timeout: 100000000,
   },
 };
+export default config;
