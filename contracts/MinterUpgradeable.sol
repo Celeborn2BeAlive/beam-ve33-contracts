f71cedb221bfdac0a40bd476a6680e684c90cc26
Celeborn2BeAlive <laurent.noel.c2ba@gmail.com>
2025-08-05 09:44:04 +0200

    Rename state variable _retro to _emissionToken

--- contracts/MinterUpgradeable.sol
+++ contracts/MinterUpgradeable.sol
@@ -24,7 +24,7 @@ contract MinterUpgradeable is IMinter, OwnableUpgradeable {
     uint public constant MAX_TEAM_RATE = 50; // 5%
 
     uint public constant WEEK = 86400 * 7; // allows minting once per week (reset every Thursday 00:00 UTC)
-    uint public weekly; // represents a starting weekly emission of 2.6M RETRO (RETRO has 18 decimals)
+    uint public weekly; // represents a starting weekly emission of 2.6M EmissionToken (EmissionToken has 18 decimals)
     uint public active_period;
     uint public constant LOCK = 86400 * 7 * 52 * 2;
 
@@ -32,7 +32,7 @@ contract MinterUpgradeable is IMinter, OwnableUpgradeable {
     address public team;
     address public pendingTeam;
 
-    IEmissionToken public _retro;
+    IEmissionToken public _emissionToken;
     IVoter public _voter;
     IVotingEscrow public _ve;
     IRewardsDistributor public _rewards_distributor;
@@ -57,7 +57,7 @@ contract MinterUpgradeable is IMinter, OwnableUpgradeable {
         TAIL_EMISSION = 2;
         REBASEMAX = 300;
 
-        _retro = IEmissionToken(IVotingEscrow(__ve).token());
+        _emissionToken = IEmissionToken(IVotingEscrow(__ve).token());
         _voter = IVoter(__voter);
         _ve = IVotingEscrow(__ve);
         _rewards_distributor = IRewardsDistributor(__rewards_distributor);
@@ -75,8 +75,8 @@ contract MinterUpgradeable is IMinter, OwnableUpgradeable {
     ) external {
         require(_initializer == msg.sender);
         if(max > 0){
-            _retro.mint(address(this), max);
-            _retro.approve(address(_ve), type(uint).max);
+            _emissionToken.mint(address(this), max);
+            _emissionToken.approve(address(_ve), type(uint).max);
             for (uint i = 0; i < claimants.length; i++) {
                 _ve.create_lock_for(amounts[i], LOCK, claimants[i]);
             }
@@ -123,7 +123,7 @@ contract MinterUpgradeable is IMinter, OwnableUpgradeable {
 
     // calculate circulating supply as total token supply - locked supply
     function circulating_supply() public view returns (uint) {
-        return _retro.totalSupply() - _retro.balanceOf(address(_ve));
+        return _emissionToken.totalSupply() - _emissionToken.balanceOf(address(_ve));
     }
 
     // emission calculation is 1% of available supply to mint adjusted by circulating / total supply
@@ -143,10 +143,10 @@ contract MinterUpgradeable is IMinter, OwnableUpgradeable {
 
     // calculate inflation and adjust ve balances accordingly
     function calculate_rebase(uint _weeklyMint) public view returns (uint) {
-        uint _veTotal = _retro.balanceOf(address(_ve));
-        uint _retroTotal = _retro.totalSupply();
+        uint _veTotal = _emissionToken.balanceOf(address(_ve));
+        uint _emissionTokenTotal = _emissionToken.totalSupply();
 
-        uint lockedShare = (_veTotal) * PRECISION  / _retroTotal;
+        uint lockedShare = (_veTotal) * PRECISION  / _emissionTokenTotal;
         if(lockedShare >= REBASEMAX){
             return _weeklyMint * REBASEMAX / PRECISION;
         } else {
@@ -173,18 +173,18 @@ contract MinterUpgradeable is IMinter, OwnableUpgradeable {
 
             uint _gauge = weekly - _rebase - _teamEmissions;
 
-            uint _balanceOf = _retro.balanceOf(address(this));
+            uint _balanceOf = _emissionToken.balanceOf(address(this));
             if (_balanceOf < _required) {
-                _retro.mint(address(this), _required - _balanceOf);
+                _emissionToken.mint(address(this), _required - _balanceOf);
             }
 
-            require(_retro.transfer(team, _teamEmissions));
+            require(_emissionToken.transfer(team, _teamEmissions));
 
-            require(_retro.transfer(address(_rewards_distributor), _rebase));
+            require(_emissionToken.transfer(address(_rewards_distributor), _rebase));
             _rewards_distributor.checkpoint_token(); // checkpoint token balance that was just minted in rewards distributor
             _rewards_distributor.checkpoint_total_supply(); // checkpoint supply
 
-            _retro.approve(address(_voter), _gauge);
+            _emissionToken.approve(address(_voter), _gauge);
             _voter.notifyRewardAmount(_gauge);
 
             emit Mint(msg.sender, weekly, circulating_supply(), circulating_emission());
