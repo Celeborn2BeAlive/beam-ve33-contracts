6728ffef27a4d46907594f2ce1209f034e2e78ed
Celeborn2BeAlive <laurent.noel.c2ba@gmail.com>
2025-08-05 16:56:41 +0200

    Add deployment scripts for new contracts

--- ignition/modules/Beam.ts
+++ ignition/modules/Beam.ts
@@ -7,6 +7,9 @@ const beamTokenName = "Beam";
 const beamTokenSymbol = "BEAM";
 export const beamTokenConstructorArgs = [beamTokenName, beamTokenSymbol]
 
+const beamAlgebraFactory = getAddress("0x28b5244B6CA7Cb07f2f7F40edE944c07C2395603")
+const wzetaAddress = getAddress("0x5f0b1a82749cb4e2278ec87f8bf6b618dc71a8bf");
+
 const BeamToken = buildModule("BeamToken", (m) => {
   const beamToken = m.contract("contracts/EmissionToken.sol:EmissionToken", beamTokenConstructorArgs);
 
@@ -48,15 +51,15 @@ const MinterUpgradeable = buildModule("MinterUpgradeable", (m) => {
 
   const { proxyAdmin } = m.useModule(ProxyAdmin);
 
-  const minterUpgradeable = m.contract("MinterUpgradeable", undefined, {
+  const minterImplementation = m.contract("MinterUpgradeable", undefined, {
     id: "MinterUpgradeableImplementation",
   });
-  const encodedInitializeCall = m.encodeFunctionCall(minterUpgradeable, "initialize",
+  const encodedInitializeCall = m.encodeFunctionCall(minterImplementation, "initialize",
     [epochDistributor, votingEscrow, rewardsDistributor],
   );
 
   const minterTransparentProxy = m.contract("TransparentUpgradeableProxy", [
-    minterUpgradeable,
+    minterImplementation,
     proxyAdmin,
     encodedInitializeCall,
   ]);
@@ -65,7 +68,7 @@ const MinterUpgradeable = buildModule("MinterUpgradeable", (m) => {
 
   const minterProxy = m.contractAt("MinterUpgradeable", minterTransparentProxy)
 
-  return { proxyAdmin, minterProxy };
+  return { minterImplementation, minterProxy, proxyAdmin };
 });
 
 const Voter = buildModule("Voter", (m) => {
@@ -85,15 +88,15 @@ const EpochDistributorUpgradeable = buildModule("EpochDistributorUpgradeable", (
   const { voter } = m.useModule(Voter);
   const { proxyAdmin } = m.useModule(ProxyAdmin);
 
-  const epochDistributorUpgradeable = m.contract("EpochDistributorUpgradeable", undefined, {
+  const epochDistributorImplementation = m.contract("EpochDistributorUpgradeable", undefined, {
     id: "EpochDistributorUpgradeableImplementation",
   });
-  const encodedInitializeCall = m.encodeFunctionCall(epochDistributorUpgradeable, "initialize",
+  const encodedInitializeCall = m.encodeFunctionCall(epochDistributorImplementation, "initialize",
     [minterProxy, beamToken, voter],
   );
 
   const epochDistributorTransparentProxy = m.contract("TransparentUpgradeableProxy", [
-    epochDistributorUpgradeable,
+    epochDistributorImplementation,
     proxyAdmin,
     encodedInitializeCall,
   ]);
@@ -102,7 +105,107 @@ const EpochDistributorUpgradeable = buildModule("EpochDistributorUpgradeable", (
 
   m.call(minterProxy, "setEpochDistributor", [epochDistributorProxy,]);
 
-  return { epochDistributorProxy, proxyAdmin }
+  return { epochDistributorImplementation, epochDistributorProxy, proxyAdmin }
+});
+
+const VotingIncentivesFactory = buildModule("VotingIncentivesFactory", (m) => {
+  const { beamToken } = m.useModule(BeamToken);
+
+  const globalFactory = ZERO_ADDRESS; // Set after deployment of GlobalFactory
+  const defaultTokens = [beamToken];
+  const votingIncentivesFactory = m.contract("VotingIncentivesFactory", [globalFactory, defaultTokens]);
+
+  // TODO votingIncentivesFactory.setGlobalFactory
+
+  return { votingIncentivesFactory };
+});
+
+const GaugeFactory = buildModule("GaugeFactory", (m) => {
+  const globalFactory = ZERO_ADDRESS; // Set after deployment of GlobalFactory
+
+  const gaugeFactory = m.contract("GaugeFactory", [globalFactory,]);
+
+  // TODO gaugeFactory.setGlobalFactory
+
+  return { gaugeFactory };
+});
+
+const AlgebraVaultFactory = buildModule("AlgebraVaultFactory", (m) => {
+  const { voter } = m.useModule(Voter);
+
+  const algebraVaultFactory = m.contract("AlgebraVaultFactory", [voter, beamAlgebraFactory]);
+
+  // TODO The AlgebraVaultFactory should be set on the AlgebraFactory
+  // with AlgebraFactory.setVaultFactory
+  // It will call AlgebraVaultFactory.createVaultForPool at each new pool
+  // deployment.
+
+  // TODO For all existing pools we need to call .setCommunityVault on each one to plug-in an AlgebraVault instance.
+  // one issue with that is only the `algebraFactory` recorded in AlgebraVaultFactory can do the .createVaultForPool
+  // and it cannot be changed after AlgebraVaultFactory is instanciated.
+  // TODO Add a new role to AlgebraVaultFactory to allow the creation and assignation of AlgebraVault to an existing pool
+
+  // TODO In order to call AlgebraVaultFactory.setCommunityFee for a pool
+  // we need the AlgebraVaultFactory to have role POOLS_ADMINISTRATOR_ROLE
+  // on the AlgebraFactory.
+
+  return { algebraVaultFactory }
+});
+
+const Claimer = buildModule("Claimer", (m) => {
+  const { votingEscrow } = m.useModule(VotingEscrow);
+  const claimer = m.contract("Claimer", [votingEscrow,]);
+  return { claimer };
+});
+
+const IncentiveMakerUpgradeable = buildModule("IncentiveMaker", (m) => {
+  const { proxyAdmin } = m.useModule(ProxyAdmin);
+  const { beamToken } = m.useModule(BeamToken);
+
+  const incentiveMakerImplementation = m.contract("IncentiveMakerUpgradeable", undefined, {
+    id: "IncentiveMakerUpgradeableImplementation",
+  });
+  const encodedInitializeCall = m.encodeFunctionCall(incentiveMakerImplementation, "initialize",
+    [beamToken, wzetaAddress],
+  );
+
+  const incentiveMakerTransparentProxy = m.contract("TransparentUpgradeableProxy", [
+    incentiveMakerImplementation,
+    proxyAdmin,
+    encodedInitializeCall,
+  ]);
+
+  const incentiveMakerProxy = m.contractAt("IncentiveMakerUpgradeable", incentiveMakerTransparentProxy)
+
+  return { incentiveMakerImplementation, incentiveMakerProxy, proxyAdmin }
+});
+
+const GlobalFactory = buildModule("GlobalFactory", (m) => {
+  const { voter } = m.useModule(Voter);
+  const { beamToken } = m.useModule(BeamToken);
+  const { epochDistributorProxy } = m.useModule(EpochDistributorUpgradeable)
+  const pairFactorySolidly = ZERO_ADDRESS;
+  const { gaugeFactory } = m.useModule(GaugeFactory);
+  const { votingIncentivesFactory } = m.useModule(VotingIncentivesFactory);
+  const theNFT = ZERO_ADDRESS;
+  const { claimer } = m.useModule(Claimer);
+  const { incentiveMakerProxy } = m.useModule(IncentiveMakerUpgradeable);
+
+  // constructor(address _voter, address _thena, address _distribution, address _pfsld, address _pfalgb, address _gf, address _vif, address _theNFT, address _claimer, address _incentiveMaker)
+  const globalFactory = m.contract("GlobalFactory", [
+    voter,
+    beamToken,
+    epochDistributorProxy,
+    pairFactorySolidly,
+    beamAlgebraFactory,
+    gaugeFactory,
+    votingIncentivesFactory,
+    theNFT,
+    claimer,
+    incentiveMakerProxy,
+  ]);
+
+  return { globalFactory };
 });
 
 export default buildModule("BeamProtocol", (m) => {
@@ -110,17 +213,32 @@ export default buildModule("BeamProtocol", (m) => {
   const { votingEscrow } = m.useModule(VotingEscrow);
   const { rewardsDistributor } = m.useModule(RewardsDistributor);
   const { proxyAdmin } = m.useModule(ProxyAdmin)
-  const { minterProxy } = m.useModule(MinterUpgradeable);
+  const { minterImplementation, minterProxy } = m.useModule(MinterUpgradeable);
   const { voter } = m.useModule(Voter);
-  const { epochDistributorProxy } = m.useModule(EpochDistributorUpgradeable);
+  const { epochDistributorImplementation, epochDistributorProxy } = m.useModule(EpochDistributorUpgradeable);
+  const { votingIncentivesFactory } = m.useModule(VotingIncentivesFactory);
+  const { gaugeFactory } = m.useModule(GaugeFactory);
+  const { algebraVaultFactory } = m.useModule(AlgebraVaultFactory);
+  const { incentiveMakerImplementation, incentiveMakerProxy } = m.useModule(IncentiveMakerUpgradeable)
+  const { claimer } = m.useModule(Claimer);
+  const { globalFactory } = m.useModule(GlobalFactory);
 
   return {
+    proxyAdmin,
     beamToken,
     votingEscrow,
     rewardsDistributor,
+    minterImplementation,
     minterProxy,
     voter,
+    epochDistributorImplementation,
     epochDistributorProxy,
-    proxyAdmin,
+    votingIncentivesFactory,
+    gaugeFactory,
+    algebraVaultFactory,
+    incentiveMakerImplementation,
+    incentiveMakerProxy,
+    claimer,
+    globalFactory,
   }
 });
