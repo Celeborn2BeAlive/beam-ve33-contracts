6a7a737a4ffe64760098a0b29cd4769e052b407c
Celeborn2BeAlive <laurent.noel.c2ba@gmail.com>
2025-08-05 12:21:51 +0200

    Rename contract

--- contracts/EpochDistributor.sol
+++ contracts/EpochDistributor.sol
@@ -14,7 +14,7 @@ import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 
 /// @title Distribute weekly $THE to BSC gauges and opBNB
 /// @author Prometheus - Thena Finance
-contract EpochDistributorBSC is OwnableUpgradeable {
+contract EpochDistributor is OwnableUpgradeable {
 
     using SafeERC20Upgradeable for IERC20Upgradeable;
 
