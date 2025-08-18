810a6ff6af6bc0ae79e97184d15a6b5d83a5f152
Celeborn2BeAlive <laurent.noel.c2ba@gmail.com>
2025-08-18 17:14:17 +0200

    Add test for EpochDistributor

---
+++ test/BeamCore.EpochDistributor.ts
@@ -0,0 +1,157 @@
+import hre, { ignition } from "hardhat";
+import BeamCore, { Voter } from "../ignition/modules/Beam.Core";
+import { Address, getAddress, parseEther } from "viem";
+import { isHardhatNetwork, MAX_LOCKTIME, WEEK } from "./constants";
+import { loadFixture } from "@nomicfoundation/hardhat-toolbox-viem/network-helpers";
+import { expect } from "chai";
+import { time } from "@nomicfoundation/hardhat-toolbox-viem/network-helpers";
+import BeamSolidyDEX from "../ignition/modules/Beam.SolidyDEX";
+import BeamVe33Factories from "../ignition/modules/Beam.Ve33Factories";
+import { ZERO_ADDRESS } from "../ignition/modules/constants";
+
+const INITIAL_TOKEN_SUPPLY = parseEther("50000000");
+
+describe("BeamCore.EpochDistributor", () => {
+  before(async function () {
+    if (!isHardhatNetwork) {
+      this.skip();
+    }
+  });
+
+  const deployFixture = async () => {
+    const [deployer, user] = await hre.viem.getWalletClients();
+    const deployerAddress = getAddress(deployer.account.address);
+    const publicClient = await hre.viem.getPublicClient();
+
+    const beamCore = await ignition.deploy(BeamCore);
+    const solidlyDex = await ignition.deploy(BeamSolidyDEX);
+    const ve33Factories = await ignition.deploy(BeamVe33Factories);
+
+    const { beamToken, minterProxy, epochDistributorProxy, voter, claimer, votingEscrow } = beamCore;
+
+    // The Minter requires a non zero total supply or division by zero occurs in `calculate_rebase`:
+    await beamToken.write.mint([deployerAddress, INITIAL_TOKEN_SUPPLY]);
+    await beamToken.write.setMinter([minterProxy.address]);
+
+    // Set 0% emission to rebase and team to ease computation
+    await minterProxy.write.setRebase([0n]);
+    await minterProxy.write.setTeamRate([0n]);
+
+    const USDC = await hre.viem.deployContract("ERC20PresetMinterPauser", ["USDC", "USDC"]);
+    await USDC.write.mint([deployerAddress, 10_000_000_000n]);
+    const WETH = await hre.viem.deployContract("ERC20PresetMinterPauser", ["Wrapped Ether", "WETH"]);
+    await WETH.write.mint([deployerAddress, 42_000_000n]);
+
+    const { solidlyPairFactoryProxy } = solidlyDex;
+    await solidlyPairFactoryProxy.write.createPair([USDC.address, beamToken.address, false]);
+    await solidlyPairFactoryProxy.write.createPair([WETH.address, beamToken.address, false]);
+    await solidlyPairFactoryProxy.write.createPair([WETH.address, USDC.address, false]);
+    const USDC_BEAM = await solidlyPairFactoryProxy.read.getPair([USDC.address, beamToken.address, false]);
+    const WETH_BEAM = await solidlyPairFactoryProxy.read.getPair([WETH.address, beamToken.address, false]);
+    const WETH_USDC = await solidlyPairFactoryProxy.read.getPair([USDC.address, WETH.address, false]);
+
+    const { algebraVaultFactory, gaugeFactory, votingIncentivesFactory } = ve33Factories;
+    const testIncentiveMaker = await hre.viem.deployContract("TestIncentiveMaker", [beamToken.address]);
+
+    const createGaugeForPool = async (poolAddr: Address) => {
+      await algebraVaultFactory.write.createVaultForPool([poolAddr]);
+      const feeVault = await algebraVaultFactory.read.poolToVault([poolAddr]);
+      await gaugeFactory.write.createEternalGauge([poolAddr, epochDistributorProxy.address, feeVault, ZERO_ADDRESS, testIncentiveMaker.address]);
+      const events = await gaugeFactory.getEvents.CreateGauge();
+      const gaugeAddr = events[0].args.gauge as Address;
+
+      await votingIncentivesFactory.write.createVotingIncentives([USDC.address, beamToken.address, voter.address, gaugeAddr, claimer.address]);
+      const votingIncentivesAddr = await votingIncentivesFactory.read.last_votingIncentives();
+      await gaugeFactory.write.setVotingIncentives([gaugeAddr, votingIncentivesAddr]);
+
+      await voter.write.addPoolData([poolAddr, gaugeAddr, votingIncentivesAddr]);
+
+      return {
+        feeVault,
+        gaugeAddr,
+        votingIncentivesAddr,
+      };
+    };
+    await createGaugeForPool(USDC_BEAM);
+    await createGaugeForPool(WETH_BEAM);
+    await createGaugeForPool(WETH_USDC);
+
+    await minterProxy.write._initialize([[], [], 0n]);
+    const activePeriod = await minterProxy.read.active_period();
+
+    // Lock 10% of total supply
+    const totalSupply = await beamToken.read.totalSupply();
+    const depositAmount = totalSupply / 10n;
+    await beamToken.write.approve([votingEscrow.address, depositAmount]);
+
+    await votingEscrow.write.create_lock([depositAmount, MAX_LOCKTIME]);
+    const events = await votingEscrow.getEvents.Transfer();
+    const veNFTId = events[0].args.tokenId as bigint;
+
+    return {
+      publicClient,
+      deployer,
+      user,
+      deployerAddress,
+      testIncentiveMaker,
+      activePeriod,
+      veNFTId,
+      USDC,
+      WETH,
+      ...beamCore,
+      ...solidlyDex,
+      pools: {
+        USDC_BEAM,
+        WETH_BEAM,
+        WETH_USDC,
+      },
+    };
+  };
+
+  const simulateOneWeek = async (activePeriod: bigint) => {
+    const nextPeriod = activePeriod + WEEK;
+    await time.setNextBlockTimestamp(activePeriod + WEEK);
+    return { nextPeriod };
+  };
+
+  it("Should distribute farming rewards to gauges", async () => {
+    const { deployerAddress, minterProxy, activePeriod, beamToken, epochDistributorProxy, testIncentiveMaker, voter, veNFTId, pools } = await loadFixture(deployFixture);
+
+    const votes = {
+      [pools.USDC_BEAM]: 50n,
+      [pools.WETH_BEAM]: 35n,
+      [pools.WETH_USDC]: 15n,
+    } as {[key: Address]: bigint};
+
+    await voter.write.vote([veNFTId, Object.keys(votes) as [Address], Object.values(votes)]);
+
+    await simulateOneWeek(activePeriod);
+    await minterProxy.write.update_period();
+
+    const expectedEmission = await minterProxy.read.weekly();
+
+    expect(await beamToken.read.balanceOf([epochDistributorProxy.address])).to.equals(expectedEmission);
+
+    const [amountEpoch0, totalWeightsEpoch0, timestampEpoch0, poolsLengthEpoch0] = await epochDistributorProxy.read.amountsPerEpoch([0n]);
+
+    expect(amountEpoch0).to.equals(expectedEmission);
+    expect(timestampEpoch0).to.equals(activePeriod);
+    expect(totalWeightsEpoch0).to.equals(await voter.read.totalWeights([activePeriod]));
+    expect(poolsLengthEpoch0).to.equals(3n);
+
+    await epochDistributorProxy.write.setAutomation([deployerAddress, true]);
+    await epochDistributorProxy.write.distributeAll();
+
+    // We expect a small token lefhover in the epoch distributor because of integer division
+    const lefthoverAmount = await beamToken.read.balanceOf([epochDistributorProxy.address]);
+    const distributedAmount = await beamToken.read.balanceOf([testIncentiveMaker.address]);
+    expect(expectedEmission).to.equals(distributedAmount + lefthoverAmount);
+    expect(lefthoverAmount < distributedAmount * 10n / 1000n).to.be.true; // Arbitrary check: less than 0.1% lefthover
+
+    Object.entries(votes).forEach(async ([poolAddr, vote]) => {
+      expect(await testIncentiveMaker.read.poolAmount([poolAddr as Address])).to.equals(
+        distributedAmount * vote / 100n
+      );
+    });
+  });
+});
