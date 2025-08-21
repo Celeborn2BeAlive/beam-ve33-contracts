8ea97816fa6321092792bdaa774948c18102017d
Celeborn2BeAlive <laurent.noel.c2ba@gmail.com>
2025-08-21 17:56:44 +0200

    Remove old unused Retro contracts

--- retro_contracts/interfaces/IHypervisor.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.13;
-
-interface IHypervisor {
-    function pool() external view returns(address);
-    function getTotalAmounts() external view returns(uint tot0,uint tot1);
-}
--- retro_contracts/interfaces/IPairInfo.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.13;
-
-interface IPairInfo {
-
-    function token0() external view returns(address);
-    function token1() external view returns(address);
-    function fee() external view returns(uint24);
-}
